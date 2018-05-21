8dc80429b9cfec2ec3017e7136df277bcada83a6
catohaste <catohaste@users.noreply.github.com>
2018-05-21 11:35:26 +0100

    Minor bug fix.

--- functions/testCellSize/cellWithBorderSizeParams.m
+++ functions/testCellSize/cellWithBorderSizeParams.m
@@ -10,15 +10,15 @@ cellMask    = boundingBoxToCellMask(redBW,boundingBox);
 redImage = imadjust(redBW);
 yelImage = imadjust(im2double(imread(imageStruct.yelPath)));
 
-[redImX,map]   = gray2ind(redImage,256);
-redMap      = [map(:,1),zeros(256,1),zeros(256,1)];
-redImage       = ind2rgb(redImX,redMap);
-redCropped     = boundingBoxToCroppedColor(redImage,boundingBox);
+[redImX,tmpRedMap]	= gray2ind(redImage,256);
+redMap							= [tmpRedMap(:,1),zeros(256,1),zeros(256,1)];
+redImage						= ind2rgb(redImX,redMap);
+redCropped					= boundingBoxToCroppedColor(redImage,boundingBox);
 
-[yelImX,tmpYelMap] = gray2ind(yelImage,256);
-yelMap = [tmpYelMap(:,1),tmpYelMap(:,1),zeros(256,1)];
-yelImage = ind2rgb(yelImX,yelMap);
-yelCropped = boundingBoxToCroppedColor(yelImage,boundingBox);
+[yelImX,tmpYelMap]	= gray2ind(yelImage,256);
+yelMap							= [tmpYelMap(:,1),tmpYelMap(:,1),zeros(256,1)];
+yelImage						= ind2rgb(yelImX,yelMap);
+yelCropped					= boundingBoxToCroppedColor(yelImage,boundingBox);
 
 edgeCellMask = edge(cellMask);
 redCellImage = imoverlay(redCropped,edgeCellMask,'w');
