892520531991b271dd93392cc2c1876e9f567092
catohaste <catohaste@users.noreply.github.com>
2018-06-26 13:10:31 +0100

    Input file names changes. Committing to get rid of conflicts.

--- MainQuench_CFTRimg.m
+++ MainQuench_CFTRimg.m
@@ -5,8 +5,8 @@ close all
 
 addpath(genpath('functions'))
 
-addpath(genpath(fullfile('~','Desktop','CFTR','input'))) % location of your input folder
-inputCatoNPPBQuench % the name of your input file
+addpath(genpath(fullfile('~/Desktop/CFTR','input'))) % location of your input folder
+inputCatoNPPBForskolinQuench % the name of your input file
 
 %% STRUCTURING DATA
 tic
