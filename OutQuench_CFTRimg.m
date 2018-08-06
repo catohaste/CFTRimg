cba7275114b33bbf78d34eb77930e697f1a019c4
catohaste <catohaste@users.noreply.github.com>
2018-08-06 12:42:43 +0100

    Minor change.

--- OutQuench_CFTRimg.m
+++ OutQuench_CFTRimg.m
@@ -5,10 +5,10 @@ close all
 figure
 counter = 1;
 
-% subplotRows = 2;
-% subplotCols = 4;
-% conditionIdx = 1:7;
-% subplotIdx = [1,2,3,5,6,7,8];
+subplotRows = 2;
+subplotCols = 4;
+conditionIdx = 1:7;
+subplotIdx = [1,2,3,5,6,7,8];
 
 % conditionIdx = 1:12;
 % subplotIdx = 1:12;
@@ -20,10 +20,15 @@ counter = 1;
 % subplotRows = 3;
 % subplotCols = 3;
 
-conditionIdx = 1:4;
-subplotIdx = [1,2,3,4];
-subplotRows = 2;
-subplotCols = 3;
+% conditionIdx = 1:4;
+% subplotIdx = [1,2,3,4];
+% subplotRows = 2;
+% subplotCols = 3;
+
+% conditionIdx = 1:6;
+% subplotIdx = 1:6;
+% subplotRows = 2;
+% subplotCols = 3;
 
 for i=conditionIdx
 	  subplot(subplotRows,subplotCols,subplotIdx(i))
